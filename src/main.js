9e0641554bce43a4b3428ce5a75adb7c51b39bde
Irena Karpan <irenakarpan@gmail.com>
2026-07-24 18:15:12 +0200

    add form styles

--- src/main.js
+++ src/main.js
@@ -2,12 +2,25 @@
 
 import getImagesByQuery from './js/pixabay-api';
 
+import errorIcon from './img/error.svg'
+
 // iziToast
 import iziToast from "izitoast";
 // styles
 import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector('.form');
+
+const iziErrorOptions = {
+    title: "Error",
+    backgroundColor:'#EF4040',
+    iconUrl: errorIcon,
+    position: 'topRight',
+    titleColor: '#FFFFFF',
+    messageColor: '#FFFFFF',
+    iconColor: '#FFFFFF',
+    timeout: 5000,
+};
 
 form.addEventListener('submit', requestHandler);
 
@@ -17,10 +30,16 @@ function requestHandler(event) {
     const input = event.target.elements['search-text'];
 
     if (!input.value.trim()) {
-        iziToast.error();
+        iziToast.error({
+            ...iziErrorOptions,
+            message: "Empty data. Please try again!"
+        });
     }
     
     getImagesByQuery(input.value.trim())
     .then(response => console.log(response))
-        .catch(error => iziToast.error());
+        .catch(error =>  iziToast.error({
+        ...iziErrorOptions,
+        message: "Sorry, there are no images matching your search query. Please try again!",
+    }))
 }
